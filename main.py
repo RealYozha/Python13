b7c6b4b83589e47dc9c92f529ae0c61d9d4dbbb9
RealYozha <ItzYozha@ya.ru>
2023-11-03 17:33:30 +0300

    1

---
+++ main.py
@@ -0,0 +1,27 @@
+import pdfplumber
+from gtts import gTTS
+
+file_path = "text.pdf"
+pages_pdf_text = []
+pdf_text = ""
+
+
+def get_text():
+    with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        [pages_pdf_text.append(pdf_file.pages[page_index].extract_text()) for page_index in range(len(pdf_file.pages))]
+        pdf_text = ". ".join(pages_pdf_text)
+        return pdf_text.replace('\n', ' ')
+
+
+def make_audio_file(text, lang):
+    audio = gTTS(text=text, lang=lang)
+    audio.save("text.mp3")
+
+
+def main():
+    print(get_text())
+    make_audio_file(get_text(), 'ru')
+
+
+if __name__ == '__main__':
+    main()
