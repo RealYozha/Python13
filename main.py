33684025e7f97bccd83e1a9bfc181f05bcf6bd1a
RealYozha <ItzYozha@ya.ru>
2023-11-10 18:21:24 +0300

    2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pdfplumber
 from gtts import gTTS
 
-file_path = "text.pdf"
-pages_pdf_text = []
-pdf_text = ""
-
 
 def get_text():
+    file_path = "text.pdf"
     with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        pages_pdf_text = []
+        pdf_text = ""
         [pages_pdf_text.append(pdf_file.pages[page_index].extract_text()) for page_index in range(len(pdf_file.pages))]
         pdf_text = ". ".join(pages_pdf_text)
         return pdf_text.replace('\n', ' ')
